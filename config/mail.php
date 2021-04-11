ee13d4c7234fff8f5302a78453c5a92ac47ebf4d
JenniferRuiz2 <jennifer_ruiz@arpan.com.mx>
2021-04-11 00:40:26 -0500

    enviar correo de recuperación de contraseña

--- config/mail.php
+++ config/mail.php
@@ -1,4 +1,12 @@
 <?php
+//phpMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../phpMailer/Exception.php';
+require '../phpMailer/PHPMailer.php';
+require '../phpMailer/SMTP.php';
+
 // Varios destinatarios
 $para  = $email . ', '; // atención a la coma
 //$para .= 'wez@example.com';
@@ -25,21 +33,33 @@ $mensaje = '
 </html>
 ';
 
-// Para enviar un correo HTML, debe establecerse la cabecera Content-type
-$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$cabeceras .= 'From: Recuperar contraseña <cumples@example.com>' . "\r\n";
-/*
-// Cabeceras adicionales
-$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
-$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
-$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";*/
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-// Enviarlo
-$enviado = false;
-if(mail($para, $título, $mensaje, $cabeceras)){
-    $enviado = true;
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jennifer02598@gmail.com';  //Acceso                   //SMTP username
+    $mail->Password   = 'Dnlhmr02598';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('jennifer02598@gmail.com', 'Lumina optica'); //Desde donde se envia
+    $mail->addAddress($email);     //Add a recipient //A quien se le envia
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Recuperar contraseña de Lumina Optica';
+    $mail->Body    = $mensaje;
+
+    $mail->send();
+    echo 'El correo se ha enviado';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
 ?>
